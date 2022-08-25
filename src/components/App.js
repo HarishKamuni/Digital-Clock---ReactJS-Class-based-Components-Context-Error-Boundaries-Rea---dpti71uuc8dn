bdf601feae4d8cb0e94e7ea6a2ae31d66600fdc7
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2022-08-25 22:19:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 import '../styles/App.css';
-const App = () => {
 
+const App = () => {
+  let clock = new Date().toLocaleString();
+  let [time,settime] = useState(clock);
+  function digital(){
+    clock = new Date().toLocaleString();
+    settime(clock)
+  }
+
+  setInterval(digital,1000)
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{time}</div>
     </div>
   )
 }
